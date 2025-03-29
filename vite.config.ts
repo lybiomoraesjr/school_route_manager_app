1f4fb0f28036c530df9cd703ab3336b53c0092db
Lybio Moraes Junior <j.lybio@aluno.ifsp.edu.br>
2025-03-29 11:11:07 -0300

    chore: setup aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "node:path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@hooks": path.resolve(__dirname, "./src/hooks"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@context": path.resolve(__dirname, "./src/context"),
+      "@services": path.resolve(__dirname, "./src/services"),
+      "@validators": path.resolve(__dirname, "./src/validators"),
+      "@utils": path.resolve(__dirname, "./src/utils"),
+      "@theme": path.resolve(__dirname, "./src/theme"),
+      "@enums": path.resolve(__dirname, "./src/enums"),
+      "@types": path.resolve(__dirname, "./src/types"),
+    },
+  },
+});
