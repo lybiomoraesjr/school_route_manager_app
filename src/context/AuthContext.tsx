e0c11c7e7685413d6d0d29dd367e3a5d0868e749
Lybio Moraes Junior <j.lybio@aluno.ifsp.edu.br>
2025-03-29 16:03:02 -0300

    chore: add user context

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,28 @@
+import { UserDTO } from "@/dtos/UserDTO";
+import { createContext, ReactNode, useState } from "react";
+
+export type AuthContextDataProps = {
+  user: UserDTO;
+};
+
+export const AuthContext = createContext<AuthContextDataProps>(
+  {} as AuthContextDataProps
+);
+
+type AuthContextProviderProps = {
+  children: ReactNode;
+};
+
+export const AuthContextProvider = ({ children }: AuthContextProviderProps) => {
+  const [user, setUser] = useState<UserDTO>({} as UserDTO);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
