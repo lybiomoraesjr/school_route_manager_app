88b8b79b0158945df82be8f046d894aa6502ba9b
Lybio Moraes Junior <j.lybio@aluno.ifsp.edu.br>
2025-03-30 19:26:18 -0300

    chore: add example component

---
+++ src/shared/components/Example/Example.tsx
@@ -0,0 +1,10 @@
+import "./Example.module.css";
+
+export const Example = () => {
+  return (
+    <div>
+      <h1>Example Component</h1>
+      <p>This is an example component.</p>
+    </div>
+  );
+};
