8080f4dcc0bf2ddb0aff21d8a8dc34b8e053de46
Lybio Moraes Junior <j.lybio@aluno.ifsp.edu.br>
2025-05-22 21:37:22 -0300

    test: add roles object

--- src/shared/layouts/Layout/components/Sidebar/Sidebar.tsx
+++ src/shared/layouts/Layout/components/Sidebar/Sidebar.tsx
@@ -13,36 +13,89 @@ import { useNavigate } from "react-router-dom";
 
 const ICON_SIZE = 20;
 
-const links = [
-	{ label: "Dashboard", icon: <House size={ICON_SIZE} />, route: "/home" },
-	{
-		label: "Motoristas",
-		icon: <Users size={ICON_SIZE} />,
-		route: "/motoristas",
-	},
-	{ label: "Rotas", icon: <MapTrifold size={ICON_SIZE} />, route: "/rotas" },
-	{ label: "Alunos", icon: <Student size={ICON_SIZE} />, route: "/alunos" },
-	{
-		label: "Escolas",
-		icon: <Buildings size={ICON_SIZE} />,
-		route: "/escolas",
-	},
-	{ label: "Paradas", icon: <MapPin size={ICON_SIZE} />, route: "/paradas" },
-	{
-		label: "Monitores",
-		icon: <UserCircle size={ICON_SIZE} />,
-		route: "/monitores",
-	},
-	{ label: "Veículos", icon: <Bus size={ICON_SIZE} />, route: "/veiculos" },
-	{
-		label: "Responsáveis",
-		icon: <UserCircle size={ICON_SIZE} />,
-		route: "/responsaveis",
-	},
-];
+enum roles {
+	ADMIN = "admin",
+	SCHOOL = "school",
+	MONITOR = "monitor",
+}
 
-export const Sidebar = () => {
+const RolesLinks = {
+	[roles.ADMIN]: [
+		{
+			label: "Dashboard",
+			icon: <House size={ICON_SIZE} />,
+			route: "/home",
+		},
+		{
+			label: "Motoristas",
+			icon: <Users size={ICON_SIZE} />,
+			route: "/motoristas",
+		},
+		{
+			label: "Rotas",
+			icon: <MapTrifold size={ICON_SIZE} />,
+			route: "/rotas",
+		},
+		{
+			label: "Alunos",
+			icon: <Student size={ICON_SIZE} />,
+			route: "/alunos",
+		},
+		{
+			label: "Escolas",
+			icon: <Buildings size={ICON_SIZE} />,
+			route: "/escolas",
+		},
+		{
+			label: "Paradas",
+			icon: <MapPin size={ICON_SIZE} />,
+			route: "/paradas",
+		},
+		{
+			label: "Monitores",
+			icon: <UserCircle size={ICON_SIZE} />,
+			route: "/monitores",
+		},
+		{
+			label: "Veículos",
+			icon: <Bus size={ICON_SIZE} />,
+			route: "/veiculos",
+		},
+		{
+			label: "Responsáveis",
+			icon: <UserCircle size={ICON_SIZE} />,
+			route: "/responsaveis",
+		},
+	],
+	[roles.SCHOOL]: [
+		{
+			label: "Escolas",
+			icon: <Buildings size={ICON_SIZE} />,
+			route: "/escolas",
+		},
+	],
+
+	[roles.MONITOR]: [
+		{
+			label: "Monitores",
+			icon: <UserCircle size={ICON_SIZE} />,
+			route: "/monitores",
+		},
+		{
+			label: "Veículos",
+			icon: <Bus size={ICON_SIZE} />,
+			route: "/veiculos",
+		},
+	],
+};
+
+interface SidebarProps {
+	role?: roles;
+}
+
+export const Sidebar = ({ role = roles.ADMIN }: SidebarProps) => {
 	const navigate = useNavigate();
+	const links = RolesLinks[role] || [];
 
 	return (
 		<Stack>
