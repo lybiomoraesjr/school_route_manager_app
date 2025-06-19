4beee52cd319603b6d2f510c06a621130167de08
Lybio Moraes Junior <j.lybio@aluno.ifsp.edu.br>
2025-06-18 21:44:05 -0300

    feat: configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,20 +4,24 @@ import path from "node:path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "@components": path.resolve(__dirname, "./src/components"),
-      "@hooks": path.resolve(__dirname, "./src/hooks"),
-      "@pages": path.resolve(__dirname, "./src/pages"),
-      "@context": path.resolve(__dirname, "./src/context"),
-      "@services": path.resolve(__dirname, "./src/services"),
-      "@validators": path.resolve(__dirname, "./src/validators"),
-      "@utils": path.resolve(__dirname, "./src/utils"),
-      "@theme": path.resolve(__dirname, "./src/theme"),
-      "@enums": path.resolve(__dirname, "./src/enums"),
-      "@types": path.resolve(__dirname, "./src/types"),
-    },
-  },
+	plugins: [react()],
+	base:
+		process.env.NODE_ENV === "production"
+			? "/school_route_manager_webapp/"
+			: "/",
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+			"@components": path.resolve(__dirname, "./src/components"),
+			"@hooks": path.resolve(__dirname, "./src/hooks"),
+			"@pages": path.resolve(__dirname, "./src/pages"),
+			"@context": path.resolve(__dirname, "./src/context"),
+			"@services": path.resolve(__dirname, "./src/services"),
+			"@validators": path.resolve(__dirname, "./src/validators"),
+			"@utils": path.resolve(__dirname, "./src/utils"),
+			"@theme": path.resolve(__dirname, "./src/theme"),
+			"@enums": path.resolve(__dirname, "./src/enums"),
+			"@types": path.resolve(__dirname, "./src/types"),
+		},
+	},
 });
